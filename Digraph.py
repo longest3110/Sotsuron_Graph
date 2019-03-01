216a1b60072a3abf14879f34dcbf181299a3d80a
Saito_Hiroki <longest3110@gmail.com>
2019-03-02 01:02:36 +0900

    [issue #3] DB処理追加

--- Digraph.py
+++ Digraph.py
@@ -1,20 +1,26 @@
 from graphviz import Digraph
+import sqlite3
+
+connection = sqlite3.connect('Jukugo.db')
+cursor = connection.cursor()
 
 # formatはpngを指定(他にはPDF, PNG, SVGなどが指定可)
 G = Digraph(format='png')
 G.attr('node', shape='circle')
 
+# ルートのノード
+root = "学"
+
+# ルートのノード生成
+G.node(root, shape='box')
 
 
-# ノードの追加
-G.node("本")
-G.node("当")
-
-# 辺の追加
-G.edge("本", "当")
 
 # print()するとdot形式で出力される
 print(G)
 
 # binary_tree.pngで保存
 G.render('binary_tree')
+
+connection.commit()
+connection.close()
