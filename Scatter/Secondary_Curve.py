43ecb0706094c3774f4a68271d22015206ccab8f
Saito_Hiroki <longest3110@gmail.com>
2019-03-05 22:12:11 +0900

    [issue #1] 軸ラベルを追加(多項式曲線)

--- Scatter/Secondary_Curve.py
+++ Scatter/Secondary_Curve.py
@@ -4,8 +4,10 @@ import pandas as pd
 
 data = pd.read_csv("data.csv")
 columns = list(data.columns)
+labels = {'vertex':"辺の本数 [本]", 'answer':"解の個数 [個]", 'in_vetex_max':"入次数の最大値 [本]", 'out_vertex_max':"出次数の最大値 [本]", 'multiple':"多重辺の本数 [本]", 'time_ave':"解答時間 [秒]", 'respondent':"完答率 [%]"}
 
-y = np.array(data['time_ave'])
+ylabel = 'time_ave'
+y = np.array(data[ylabel])
 for c in columns:
 	fig = plt.figure(c)
 	x = np.array(data[c])
@@ -15,6 +17,8 @@ for c in columns:
 	y2 = np.poly1d(res)(x2)
 
 	plt.scatter(x, y)
+	plt.xlabel(labels[c])
+	plt.ylabel(labels[ylabel])
 	plt.plot(x2, y2, color='black')
 
 	fig.savefig('graph/Secondary_Curve/{0}.png'.format(c))
