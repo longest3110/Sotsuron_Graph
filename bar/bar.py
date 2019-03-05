3de0ace506d44c3e752cf310d4ec8644e1546daa
Saito_Hiroki <longest3110@gmail.com>
2019-03-06 00:58:49 +0900

    [develop] 見た目の修正

--- bar/bar.py
+++ bar/bar.py
@@ -44,23 +44,23 @@ no_loop_data = np.array(tuple(analyzed_cursor.execute(quely)))
 print(no_loop_data)
 
 fig = plt.figure("time")
-left = np.array([1, 2])
-label = ["含む", "含まない"]
-height = np.array([loop_data[0][0], no_loop_data[0][0]])
-plt.bar(left, height, tick_label=label, align="center", width=0.5)
+left = np.array([0.5, 1, 2, 2.5])
+label = ["", "含む", "含まない", ""]
+height = np.array([0, loop_data[0][0], no_loop_data[0][0], 0])
+plt.bar(left, height, tick_label=label, align="center", width=0.7)
 plt.xlabel("有向閉路グラフを含む")
 plt.ylabel("解答時間 [秒]")
-fig.savefig('time.png')
+fig.savefig('time_loop.png')
 
 
 fig = plt.figure("respondent")
-left = np.array([1, 2])
-label = ["含む", "含まない"]
-height = np.array([loop_data[0][1], no_loop_data[0][1]])
-plt.bar(left, height, tick_label=label, align="center", width=0.5)
+left = np.array([0.5, 1, 2, 2.5])
+label = ["", "含む", "含まない", ""]
+height = np.array([0, loop_data[0][1], no_loop_data[0][1], 0])
+plt.bar(left, height, tick_label=label, align="center", width=0.7)
 plt.xlabel("有向閉路グラフを含む")
 plt.ylabel("完答率 [%]")
-fig.savefig('respondent.png')
+fig.savefig('respondent_loop.png')
 
 analyzed_connection.commit()
 analyzed_connection.close()
