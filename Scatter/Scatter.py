a14445873de97de649ede3e73a5cf3d3a8a527fc
Saito_Hiroki <longest3110@gmail.com>
2019-03-05 19:21:52 +0900

    [issue #1] 近似式を出力

--- Scatter/Scatter.py
+++ Scatter/Scatter.py
@@ -9,6 +9,13 @@ y = np.array(data['time_ave'])
 for c in columns:
 	fig = plt.figure(c)
 	x = np.array(data[c])
+
+	res = np.polyfit(x, y, 2) #2次
+	x2 = np.linspace(np.amin(x), np.amax(x), 100)
+	y2 = np.poly1d(res)(x2)
+
 	plt.scatter(x, y)
+	plt.plot(x2, y2)
+
 	fig.savefig('graph/{0}.png'.format(c))
 
