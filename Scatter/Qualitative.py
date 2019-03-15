f8a85d4f79b5151f550391475789ee394f05d59c
Saito_Hiroki <longest3110@gmail.com>
2019-03-15 13:59:54 +0900

    [issue #4] x軸を反転

--- Scatter/Qualitative.py
+++ Scatter/Qualitative.py
@@ -9,7 +9,7 @@ ylabel = '解答時間 [秒]'
 y = np.array(data[ylabel])
 for c in ['最初が確定', '最後が確定', '全域サイクルを含む']:
 	fig = plt.figure(c)
-	x = np.array(data[c])
+	x = np.array([(a + 1) % 2 for a in data[c]])
 
 	plt.scatter(x, y)
 	plt.xlabel(c)
@@ -17,9 +17,9 @@ for c in ['最初が確定', '最後が確定', '全域サイクルを含む']:
 	plt.xlim(-1, 2)
 
 	if c != '全域サイクルを含む':
-		plt.xticks([0, 1], ['していない', 'している'])
+		plt.xticks([0, 1], ['している', 'していない'])
 	else:
-		plt.xticks([0, 1], ['含まない', '含む'])
+		plt.xticks([0, 1], ['含む', '含まない'])
 
 	fig.savefig('graph/Qualitative/{0}.png'.format(c))
 
