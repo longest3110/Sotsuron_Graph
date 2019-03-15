ba7307e93c7e60592e761984b3a522619173a3f4
Saito_Hiroki <longest3110@gmail.com>
2019-03-15 13:52:51 +0900

    [issue #4] 散布図を出力

--- Scatter/Qualitative.py
+++ Scatter/Qualitative.py
@@ -2,24 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv("data.csv")
+data = pd.read_csv("Qualitative_data.csv")
 columns = list(data.columns)
-labels = {'vertex':"辺の本数 [本]", 'answer':"解の個数 [個]", 'in_vetex_max':"入次数の最大値 [本]", 'out_vertex_max':"出次数の最大値 [本]", 'multiple':"多重辺の本数 [本]", 'time_ave':"解答時間 [秒]", 'respondent':"完答率 [%]"}
 
-ylabel = 'time_ave'
+ylabel = '解答時間 [秒]'
 y = np.array(data[ylabel])
-for c in columns:
+for c in ['最初が確定', '最後が確定', '全域サイクルを含む']:
 	fig = plt.figure(c)
 	x = np.array(data[c])
 
-	res = np.polyfit(x, y, 2) #2次
-	x2 = np.linspace(np.amin(x), np.amax(x), 100)
-	y2 = np.poly1d(res)(x2)
-
 	plt.scatter(x, y)
-	plt.xlabel(labels[c])
-	plt.ylabel(labels[ylabel])
-	plt.plot(x2, y2, color='black')
+	plt.xlabel(c)
+	plt.ylabel(ylabel)
+	plt.xlim(-1, 2)
 
-	fig.savefig('graph/Secondary_Curve/{0}.png'.format(c))
+	if c != '全域サイクルを含む':
+		plt.xticks([0, 1], ['していない', 'している'])
+	else:
+		plt.xticks([0, 1], ['含まない', '含む'])
+
+	fig.savefig('graph/Qualitative/{0}.png'.format(c))
 
