c074af62734b34dffa0cb6e5f5ad8d5aa489326e
Saito_Hiroki <longest3110@gmail.com>
2019-03-17 11:34:26 +0900

    [issue #5] 近似曲線を削除

--- Scatter/Scatter.py
+++ Scatter/Scatter.py
@@ -6,20 +6,16 @@ data = pd.read_csv("data.csv")
 columns = list(data.columns)
 labels = {'vertex':"辺の本数 [本]", 'answer':"解の個数 [個]", 'in_vetex_max':"入次数の最大値 [本]", 'out_vertex_max':"出次数の最大値 [本]", 'multiple':"多重辺の本数 [本]", 'time_ave':"解答時間 [秒]", 'respondent':"完答率 [%]"}
 
-ylabel = 'time_ave'
-y = np.array(data[ylabel])
-for c in columns:
-	fig = plt.figure(c)
-	x = np.array(data[c])
 
-	res = np.polyfit(x, y, 2) #2次
-	x2 = np.linspace(np.amin(x), np.amax(x), 100)
-	y2 = np.poly1d(res)(x2)
+for ylabel in 'time_ave', 'respondent':
+	y = np.array(data[ylabel])
+	for c in columns:
+		fig = plt.figure(ylabel + c)
+		x = np.array(data[c])
 
-	plt.scatter(x, y)
-	plt.xlabel(labels[c])
-	plt.ylabel(labels[ylabel])
-	plt.plot(x2, y2, color='black')
+		plt.scatter(x, y)
+		plt.xlabel(labels[c])
+		plt.ylabel(labels[ylabel])
 
-	fig.savefig('graph/Secondary_Curve/{0}.png'.format(c))
+		fig.savefig('graph/{0}_{1}.png'.format(ylabel, c))
 
