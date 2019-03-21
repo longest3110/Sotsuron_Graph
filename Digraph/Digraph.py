6b83277ae6ed14adfcf908838b7e0f5beb467704
Saito_Hiroki <longest3110@gmail.com>
2019-03-21 23:10:14 +0900

    [fix #3] 指定した深さの木を生成

--- Digraph/Digraph.py
+++ Digraph/Digraph.py
@@ -9,17 +9,32 @@ G = Digraph(format='png')
 G.attr('node', shape='circle')
 
 # ルートのノード
-root = "学年金"
+root = "学"
+
+node_list = list(root)
 
 # ルートのノード生成
-G.node(root, shape='box')
+G.node(root)
 
-for n in tuple(cursor.execute("SELECT right_kanji FROM idioms_master WHERE left_kanji='{0}' ORDER BY RANDOM() LIMIT 10".format(root[-1]))):
-	G.node(n[0])
-	G.edge(root, n[0])
+last_node = list(root)
+
+
+for _ in range(3):
+	tmp = list()
+	for r in last_node:
+		for n in tuple(cursor.execute("SELECT right_kanji FROM idioms_master WHERE left_kanji='{0}' ORDER BY RANDOM() LIMIT 3".format(r[-1]))):
+			if not n[0] in node_list:
+				tmp.append(n[0])
+				G.node(n[0])
+				G.edge(r, n[0])
+				node_list.append(n[0])
+	
+	last_node = tmp
+
 
 # print()するとdot形式で出力される
 print(G)
+print(node_list)
 
 # binary_tree.pngで保存
 G.render('binary_tree')
